14fa58a0a9d4321d8223325ad766bbd53deac5e3
Abdulhalim7177 <abdulmuhd7177@gmail.com>
2025-05-08 13:58:36 +0100

    Update footer links and contact information

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,13 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    email: 'abdulmuhd7177@gmail.com',
+    phone: '+2349071777361',
+    address: 'Kano, Nigeria',
+    website: 'https://abdulhalim7177.github.io/',
+    github: 'https://github/Abdulhalim7177',
+    facebook: 'https://www.facebook.com/abdulhalim7177',
+    instagram: 'https://www.instagram.com/abdulhalim717',
+    linkedIn: 'https://www.linkedin.com/in/Abdulhalim7177/',
+    twitter: 'https://twitter.com/abdulhalim717',
+    stackOverflow: 'https://stackoverflow.com/users/1684076/abdulhalim7177',
     devUsername: "said7388"
 }
